be51f01d8c184dbabfea66b00a97b86f834604b6
THiragi <w1th1nmybr41n@gmail.com>
2021-04-25 11:25:31 +0900

    `pages/api/population`にprefCodeが不正だった場合の処理を追加
    `req.query.prefCode`がnumber型へ変換できない値、もしくは都道府県コードに含まれない値である場合は404エラーを返す。

--- src/pages/api/population.ts
+++ src/pages/api/population.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import client from '../../lib/api';
+import toNumber from '../../lib/toNumber';
 
 // API_KEYを秘匿したいので、Next.jsのAPIルート経由で人口データを取得する
 export default async (
@@ -9,23 +10,22 @@ export default async (
   // `prefCode`が存在しない場合は404エラー
   if (!req.query.prefCode) return res.status(404).end();
 
-  // prefCodeをstring型に整形
-  const prefCode =
-    typeof req.query.prefCode === 'string'
-      ? req.query.prefCode
-      : req.query.prefCode[0];
+  // `prefCode`をnumber型へ変換
+  const prefCode = toNumber(req.query.prefCode);
 
-  /**
-   * getPopulationByPrefecture()に渡す前に、
-   * prefCodeが都道府県コードに含まれているかチェックした方が安全
-   */
+  // 変換した`prefCode`が整数ではない場合は404エラー
+  if (Number.isInteger(prefCode)) return res.status(404).end();
+  // `prefCode`の値が都道府県コードに含まれない数字の場合は404エラー
+  if (prefCode === 0 || prefCode > 47) return res.status(404).end();
 
+  // `prefCode`を使ってRESAS APIから都道府県別人口推移データを取得
   const { result } = await client.v1.population.composition.perYear.$get({
     query: {
       cityCode: '-',
-      prefCode: parseInt(prefCode, 10),
+      prefCode,
     },
   });
+
   // 取得した人口推移データのうち`label`が`総人口`の物のみを抽出
   const total = result.data.filter(
     (composition) => composition.label === '総人口',
