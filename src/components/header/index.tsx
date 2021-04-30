a54b5e5441e1ef94ecb473e4d74c5927fd371558
THiragi <w1th1nmybr41n@gmail.com>
2021-04-30 13:44:46 +0900

    HeaderコンポーネントをReact.VFCに変更

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './index.module.scss';
 
-const Header: React.FC<{ title: string }> = ({ title }) => (
+const Header: React.VFC<{ title: string }> = ({ title }) => (
   <header className={styles.header}>
     <h1>{title}</h1>
   </header>
